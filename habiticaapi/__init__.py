dcac356cf7c4b58541b49ee354e774c911eae095
Nathanael Farley <Nathanael Farley>
2015-11-26 21:29:08 +0000

    Created Content object for Habitica things.

--- habiticaapi/__init__.py
+++ habiticaapi/__init__.py
@@ -2,3 +2,15 @@ from habitica_object import HabiticaObject
 from task import Task
 from character import Character
 
+class Content(HabiticaObject):
+    """Simple object that holds *all* objects in Habitica.
+
+    Create one of these objects and query it for information about
+    objects for purchase etc. 
+
+    """
+    def __init__(self, uuid, apikey):
+        super(Content, self).__init__(uuid, apikey, endpoint="/content")
+    
+
+
